11e5b8d98b9d54ec75403c893efa2b3152b68ca5
Alice Luca <alice.luca001@gmail.com>
2018-07-28 17:21:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var express = require("express"); 
+var bodyParser = require("body-parser");
+const server = express(); //chiamata al server
+const porta = 2000; //la porta
+const path = require('path');
+server.use(express.static("public"));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+server.listen(porta,function(){ //inserisco cosa fa il server quando lo richiamo
+console.log("server in ascolto sulla porta " + porta);
+
+});
+
+
+server.get("/", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'home.html'));
+});
+
+server.get("/chiSiamo", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'chiSiamo.html'));
+});
+server.get("/doveSiamo", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'doveSiamo.html'));
+});
+server.get("/comeFunziona", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'comeFunziona.html'));
+});
+server.get("/conChiLavoriamo", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'conChiLavoriamo.html'));
+});
+server.get("/condizioniDiVendita", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'condizioniDiVendita.html'));
+});
+server.get("/contattaci", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'contattaci.html'));
+});
+server.get("/informativaSullaPrivacy", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'informativaSullaPrivacy.html'));
+});
+server.get("/servizi", function(req,res){
+
+res.sendFile(path.join(__dirname, '/views', 'servizi.html'));
+});
