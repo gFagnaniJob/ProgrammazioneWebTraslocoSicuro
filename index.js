e3b9fea4670e2bee3463eb38dbc6c01b6759a80b
Giuseppe Fagnani <gfagnani.job@gmail.com>
2018-07-31 17:41:26 +0200

    aggiunto form registrazione e test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require("express"); 
+var express = require("express");
 var bodyParser = require("body-parser");
 const server = express(); //chiamata al server
 const porta = 2000; //la porta
@@ -8,46 +8,70 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({
     extended: true
 }));
-server.listen(porta,function(){ //inserisco cosa fa il server quando lo richiamo
-console.log("server in ascolto sulla porta " + porta);
+server.listen(porta, function () { //inserisco cosa fa il server quando lo richiamo
+    console.log("server in ascolto sulla porta " + porta);
 
 });
 
 
-server.get("/", function(req,res){
+server.get("/", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'home.html'));
+    res.sendFile(path.join(__dirname, '/views', 'home.html'));
 });
 
-server.get("/chiSiamo", function(req,res){
+server.get("/chiSiamo", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'chiSiamo.html'));
+    res.sendFile(path.join(__dirname, '/views', 'chiSiamo.html'));
 });
-server.get("/doveSiamo", function(req,res){
+server.get("/doveSiamo", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'doveSiamo.html'));
+    res.sendFile(path.join(__dirname, '/views', 'doveSiamo.html'));
 });
-server.get("/comeFunziona", function(req,res){
+server.get("/comeFunziona", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'comeFunziona.html'));
+    res.sendFile(path.join(__dirname, '/views', 'comeFunziona.html'));
 });
-server.get("/conChiLavoriamo", function(req,res){
+server.get("/conChiLavoriamo", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'conChiLavoriamo.html'));
+    res.sendFile(path.join(__dirname, '/views', 'conChiLavoriamo.html'));
 });
-server.get("/condizioniDiVendita", function(req,res){
+server.get("/condizioniDiVendita", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'condizioniDiVendita.html'));
+    res.sendFile(path.join(__dirname, '/views', 'condizioniDiVendita.html'));
 });
-server.get("/contattaci", function(req,res){
+server.get("/contattaci", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'contattaci.html'));
+    res.sendFile(path.join(__dirname, '/views', 'contattaci.html'));
 });
-server.get("/informativaSullaPrivacy", function(req,res){
+server.get("/informativaSullaPrivacy", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'informativaSullaPrivacy.html'));
+    res.sendFile(path.join(__dirname, '/views', 'informativaSullaPrivacy.html'));
 });
-server.get("/servizi", function(req,res){
+server.get("/servizi", function (req, res) {
 
-res.sendFile(path.join(__dirname, '/views', 'servizi.html'));
+    res.sendFile(path.join(__dirname, '/views', 'servizi.html'));
 });
+
+server.get('/registrati', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views', 'registrati.html'));
+});
+
+server.post('/registrati/locale', function(req, res) {
+    var User = {
+        nome: req.body.nome,
+        cognome: req.body.cognome,
+        dataNascita: req.body.dataNascita,
+        indirizzo: {
+            via: req.body.via,
+            stato: req.body.stato,
+            citta: req.body.citta,
+            provincia: req.body.provincia,
+            cap: req.body.cap
+        },
+        telefono: req.body.telefono,
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    console.log(User);
+})
