0dc40ab7dce1d0e6c4ee83339aac31ce6306096d
Giuseppe Fagnani <gfagnani.job@gmail.com>
2018-08-08 16:20:49 +0200

    sistemata organizzazione per mvc

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const server = express(); //chiamata al server
 const porta = 2000; //la porta
 const path = require('path');
 var userController = require("./controllers/user.js");
+const UserModel = require('./models/user');
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
 var bcrypt = require('bcrypt');
 server.use(express.static("public"));
 server.use(bodyParser.json());
@@ -16,37 +17,7 @@ server.use(bodyParser.urlencoded({
 
 server.set('view engine', 'ejs');
 
-
-var utentiSchema = new Schema({
-    nome: { type: String, required: true, max: 100 },
-    cognome: { type: String, required: true, max: 100 },
-
-    /*
-        via: { type: String, required: true, max: 100 },
-        provincia: { type: String, required: true, max: 150 },
-        stato: { type: String, required: true, max: 150 },
-        citta: { type: String, required: true, max: 150 },
-        cap: { type: String, required: true, max: 10 },
-    */
-
-    indirizzo: {
-        via: { type: String },
-        provincia: { type: String },
-        stato: { type: String },
-        citta: { type: String },
-        cap: { type: String }
-    },
-    dataNascita: { type: Date, required: false },
-    telefono: { type: String, required: true, max: 100 },
-    email: { type: String, required: true, max: 100 },
-    password: { type: String, required: true, max: 100 },
-
-});
-
-
-
 var mongoDB = 'mongodb://127.0.0.1/traslocosicuro';
-var modelloUtenti = mongoose.model('utenti', utentiSchema);
 
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -61,50 +32,50 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 
-server.listen(porta, function() { //inserisco cosa fa il server quando lo richiamo
+server.listen(porta, function () { //inserisco cosa fa il server quando lo richiamo
     console.log("server in ascolto sulla porta " + porta);
 
 });
 
 
-server.get("/", function(req, res) {
+server.get("/", function (req, res) {
     res.render('home');
 });
 
-server.get("/chiSiamo", function(req, res) {
+server.get("/chiSiamo", function (req, res) {
     res.render('chiSiamo');
 });
-server.get("/doveSiamo", function(req, res) {
+server.get("/doveSiamo", function (req, res) {
     res.render('doveSiamo');
 });
-server.get("/comeFunziona", function(req, res) {
+server.get("/comeFunziona", function (req, res) {
     res.render('comeFunziona');
 });
-server.get("/conChiLavoriamo", function(req, res) {
+server.get("/conChiLavoriamo", function (req, res) {
     res.render('conChiLavoriamo');
 });
-server.get("/condizioniDiVendita", function(req, res) {
+server.get("/condizioniDiVendita", function (req, res) {
     res.render('condizioniDiVendita');
 });
-server.get("/contattaci", function(req, res) {
+server.get("/contattaci", function (req, res) {
     res.render('contattaci');
 });
-server.get("/informativaSullaPrivacy", function(req, res) {
+server.get("/informativaSullaPrivacy", function (req, res) {
     res.render('informativaSullaPrivacy');
 });
-server.get("/servizi", function(req, res) {
+server.get("/servizi", function (req, res) {
     res.render('servizi');
 });
 
-server.get('/registrati', function(req, res) {
+server.get('/registrati', function (req, res) {
     res.render('registrati',
-    {
-        messaggioErrore: "",
-        bootstrapClasses: ""
-    });
+        {
+            messaggioErrore: "",
+            bootstrapClasses: ""
+        });
 });
 
-server.post('/registrati/locale', function(req, res) {
+server.post('/registrati/locale', function (req, res) {
 
     var User = {
         nome: req.body.nome,
@@ -125,20 +96,20 @@ server.post('/registrati/locale', function(req, res) {
 
     if (!userController.controllaPasswordCoincidenti(User.password, User.confermaPassword)) {
         res.render('registrati',
-        {
-            messaggioErrore: "Le due password non coincidono",
-            bootstrapClasses: "text-center text-danger"
-        });
+            {
+                messaggioErrore: "Le due password non coincidono",
+                bootstrapClasses: "text-center text-danger"
+            });
         return;
     }
 
 
     if (!userController.controlloData(User.dataNascita)) {
         res.render('registrati',
-        {
-            messaggioErrore: "Non sei maggiorenne",
-            bootstrapClasses: "text-center text-danger"
-        });
+            {
+                messaggioErrore: "Non sei maggiorenne",
+                bootstrapClasses: "text-center text-danger"
+            });
         return;
     }
 
@@ -146,65 +117,34 @@ server.post('/registrati/locale', function(req, res) {
 
 
 
-    var newUser = new modelloUtenti({ nome: req.body.nome, cognome: req.body.cognome,
-                                    email: req.body.email, password: req.body.password,
-                                    indirizzo: {
-                                        provincia: req.body.provincia,
-                                        stato: req.body.stato,
-                                        citta: req.body.citta,
-                                        cap: req.body.cap,
-                                        via: req.body.via
-                                    },
-                                    dataNascita: req.body.dataNascita,
-                                    telefono: req.body.telefono, });
-    //prima di salvare i dati nel database crypto la password
-    utentiSchema.pre('save', function(next) {
-        var newUser = this;
-        bcrypt.hash(newUser.password, 10, function(err, hash) {
-            if (err) {
-                return next(err);
-            }
-            newUser.password = hash;
-            next();
-        })
+    var newUser = new UserModel(
+        {
+            nome: User.nome,
+            cognome: User.cognome,
+            indirizzo: {
+                via: User.indirizzo.via,
+                provincia: User.indirizzo.provincia,
+                stato: User.indirizzo.stato,
+                citta: User.indirizzo.citta,
+                cap: User.indirizzo.cap,
+            },            
+            dataNascita: User.dataNascita,
+            telefono: User.telefono,
+            email: User.email,
+            password: User.password
+        });
+
+    newUser.save(function (err) {
+        if (err) console.log(err);//return handleError(err);
     });
-    newUser.save(function(err) {
-        if (err) return handleError(err);
-    });
-
-    /* TODO: Aggiungere il salt
-    userSchema.pre('save', async function (next) {
-    try {
-        if (this.method !== 'local') {
-            next();
-        }
-        //Generate a salt
-        const salt = await bcrypt.genSalt(10);
-        //Generate a password hash (salt + hash)
-        const passwordHash = await bcrypt.hash(this.local.password, salt);
-        //Re-assign hashed version over original, plain text password
-        this.local.password = passwordHash;
-        next();
-    } catch (error) {
-        next(error);
-    }
-    });
-    */
-
-
-
-
-
-
-
     console.log(User);
 });
 
-server.get('/login', function(req, res) {
+server.get('/login', function (req, res) {
     res.render('login');
 })
 
-server.post('/login/locale', function(req, res) {
+server.post('/login/locale', function (req, res) {
     var dati = {
         email: req.body.email,
         password: req.body.password
---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+var utentiSchema = new Schema({
+    nome: { type: String, required: true, max: 100 },
+    cognome: { type: String, required: true, max: 100 },
+
+    /*
+        via: { type: String, required: true, max: 100 },
+        provincia: { type: String, required: true, max: 150 },
+        stato: { type: String, required: true, max: 150 },
+        citta: { type: String, required: true, max: 150 },
+        cap: { type: String, required: true, max: 10 },
+    */
+
+    indirizzo: {
+        via: { type: String, required: true },
+        provincia: { type: String, required: true },
+        stato: { type: String, required: true },
+        citta: { type: String, required: true },
+        cap: { type: String, required: true }
+    },
+    dataNascita: { type: Date, required: true },
+    telefono: { type: String, required: true, max: 100 },
+    email: { type: String, required: true, max: 100 },
+    password: { type: String, required: true, max: 100 },
+
+});
+
+//prima di salvare i dati nel database crypto la password
+utentiSchema.pre('save', function(next) {
+    var newUser = this;
+    bcrypt.hash(newUser.password, 10, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
+        newUser.password = hash;
+        next();
+    })
+});
+
+utentiSchema.methods.controllaPassword = function (passwordImmessa) {
+   //TODO
+   //controllaPassword dovrebbe verificare che la passwordImmessa e la password nel db (hashata) siano uguali
+}
+
+
+var modelloUtenti = mongoose.model('utenti', utentiSchema);
+
+module.exports = modelloUtenti;
